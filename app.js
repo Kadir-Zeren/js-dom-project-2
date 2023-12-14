6e0ed7fa99e24f023e611c57cfc0c77417192def
kadir-zeren <k4dzeren@gmail.com>
2023-12-15 00:48:56 +0300

    geliriniz

--- app.js
+++ app.js
@@ -23,6 +23,77 @@ let gelirler = 0;
 
 //? tum harcamalari saklayacak dizi (JSON)
 let harcamaListesi = [];
+
 //?Events
 
-//?Functions
+//! Formun submit butonuna basildiginda
+ekleFormu.addEventListener("submit", (e) => {
+  e.preventDefault(); //? reload'u engeller
+  gelirler = gelirler + Number(gelirInput.value); //? string eklemiyi engelledik
+
+  //? gelirlerin kalıcı olmasi icin localStorage a kopyaliyoruz
+  localStorage.setItem("gelirler", gelirler);
+
+  //? input degerini sifrladik
+  ekleFormu.reset();
+
+  //? Degisiklikleri sonuc tablosuna yazan fonks.
+  hesaplaVeGuncelle();
+});
+
+//! Sayfa her yuklendikten sonra calisan event
+window.addEventListener("load", () => {
+  //? gelirler bilgisini local storage'dan okuyarak global degiskenimize yaz
+  gelirler = Number(localStorage.getItem("gelirler"));
+
+  //? localStroge'den harcama listesini okuyarak global dizimize saklıyoruz.
+  harcamaListesi = JSON.parse(localStorage.getItem("harcamalar")) || [];
+
+  //? harcama dizisinin icindeki objleri tek tek DOMa yaziyoruz.
+  harcamaListesi.forEach((harcama) => harcamayiDomaYaz(harcama));
+
+  console.log(harcamaListesi);
+  //? Tarih inputunu bugun deger ile yukle
+  tarihInput.valueAsDate = new Date();
+
+  //? Degisen bilgileri hesapla ve DOM'a bas
+  hesaplaVeGuncelle();
+});
+
+//! harcama formu submit edildiginde calisir
+harcamaFormu.addEventListener("submit", (e) => {
+  e.preventDefault(); //? reload'u engelle
+
+  //? yeni harcama bilgileri ile bir obje olusutur
+  const yeniHarcama = {
+    id: new Date().getTime(), //? Sistem saatini (ms olarak) verir. Unique gibidir.
+    tarih: tarihInput.value,
+    alan: harcamaAlaniInput.value,
+    miktar: miktarInput.value,
+  };
+
+  //? yeni harcama objesini diziye ekle
+  harcamaListesi.push(yeniHarcama);
+
+  //? dizisin son halini localStorage e gonder.
+  localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi));
+
+  harcamayiDomaYaz(yeniHarcama);
+
+  hesaplaVeGuncelle();
+
+  //? Formdaki verileri sil
+  harcamaFormu.reset();
+  tarihInput.valueAsDate = new Date();
+});
+
+const hesaplaVeGuncelle = () => {
+  const giderler = harcamaListesi.reduce(
+    (toplam, harcama) => toplam + Number(harcama.miktar),
+    0
+  );
+
+  gelirinizTd.innerText = gelirler;
+  giderinizTd.innerText = giderler;
+  kalanTd.innerText = gelirler - giderler;
+};
