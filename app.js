90b7d5b5a030b845af527521873b411fb401acb7
kadir-zeren <k4dzeren@gmail.com>
2023-12-15 00:32:54 +0300

    selectors

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 //? Selectors
+const ekleBtn = document.getElementById("ekle-btn");
+const gelirInput = document.getElementById("gelir-input");
+const ekleFormu = document.getElementById("ekle-formu");
+
+//? Sonuc tablosu
+const gelirinizTd = document.getElementById("geliriniz");
+const giderinizTd = document.getElementById("gideriniz");
+const kalanTd = document.getElementById("kalan");
+
+//? harcama formu
+const harcamaFormu = document.getElementById("harcama-formu");
+const harcamaAlaniInput = document.getElementById("harcama-alani");
+const tarihInput = document.getElementById("tarih");
+const miktarInput = document.getElementById("miktar");
+
+//? Haracama Tablosu
+const harcamaBody = document.getElementById("harcama-body");
+const temizleBtn = document.getElementById("temizle-btn");
 
 //? Variables
+let gelirler = 0;
 
+//? tum harcamalari saklayacak dizi (JSON)
+let harcamaListesi = [];
 //?Events
 
 //?Functions
